0a9a81352a77fe2fb22a8c2ccdfa56901bf7441a
ChangedNameTo <william.m95@gmail.com>
2018-03-14 16:29:29 -0400

    master #comment Changed animations

--- js/filter-buttons.js
+++ js/filter-buttons.js
@@ -3,12 +3,12 @@ function filter_events( type )
     var is_hidden = $(".button." + type).attr('data-hidden') == "true";
     if( is_hidden )
     {
-        $(".label." + type).parents('.cal-card').fadeIn();
+        $(".label." + type).parents('.cal-card').show();
         $(".button." + type).attr('data-hidden', 'false');
     }
     else
     {
-        $(".label." + type).parents('.cal-card').fadeOut();
+        $(".label." + type).parents('.cal-card').hide();
         $(".button." + type).attr('data-hidden', 'true');
     }
 }
